c4bc306246b93a0372e5c89a99ef74680c30edbf
Endgame-666 <grishaevvadyusha@yandex.ru>
2025-04-17 01:47:02 +0300

    change

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -64,5 +64,6 @@ def show_category(request, cat_id):
 
     return render(request, 'women/index.html', context=data)
 
+
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
